3b18c277a59fc75ee358e7c9a8a75a80198b92c9
VuCodiing <vucodiing@gmail.com>
2021-03-23 09:27:35 +0700

    fix logo carousel in index.html

--- js/main.js
+++ js/main.js
@@ -99,10 +99,45 @@ $(".review__content").slick({
 });
 
 // logo
-$(".logoo").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
+$(".logo").slick({
   dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 376,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 // special room
@@ -110,4 +145,5 @@ $(".slick").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
+  fade: true,
 });
